86d0cef3f3964afdb1db7cb06cde55e046e137f9
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-05 12:18:24 -0300

    implement get products endpoint

--- ManagementPortal.Data/Models/BaseModel.cs
+++ ManagementPortal.Data/Models/BaseModel.cs
@@ -4,6 +4,6 @@ namespace ManagementPortal.Data.Models
   {
     public int Id { get; set; }
     public DateTime Created { get; set; }
-    public DateTime? Updated { get; set; }
+    public DateTime Updated { get; set; }
   }
 }
--- ManagementPortal.Web/Controller/ProductController.cs
+++ ManagementPortal.Web/Controller/ProductController.cs
@@ -1,4 +1,9 @@
+using System.Linq;
+using ManagementPortal.Data.Models;
+using ManagementPortal.Services.Product;
+using ManagementPortal.Web.Serialization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace ManagementPortal.Web.Controller
 {
@@ -6,6 +11,25 @@ namespace ManagementPortal.Web.Controller
   [Route("api/[controller]")]
   public class ProductController : ControllerBase
   {
+    private readonly IProductService _productService;
+    private readonly ILogger<ProductController> _logger;
 
+    public ProductController(ILogger<ProductController> logger, IProductService productService)
+    {
+      _logger = logger;
+      _productService = productService;
+    }
+
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+      _logger.LogInformation("Getting all Products");
+      var products = _productService.GetProducts();
+
+      var productViewModels = products
+        .Select(ProductMapper.SerializeProductModel);
+
+      return Ok(productViewModels);
+    }
   }
 }
