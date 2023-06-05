572ef20636483c820024fa7790ea9f79f5f08619
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-05 11:27:26 -0300

    update DbContext

--- ManagementPortal.Data/DataDbContext.cs
+++ ManagementPortal.Data/DataDbContext.cs
@@ -1,3 +1,4 @@
+using ManagementPortal.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +14,12 @@ namespace ManagementPortal.Data
     {
     }
 
-    public DbSet<Models.Customer> Customers { get; set; }
-    public DbSet<Models.CustomerAddress> CustomerAddresses { get; set; }
+    public virtual DbSet<Customer> Customers { get; set; }
+    public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+    public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<ProductInventory> ProductInventories { get; set; }
+    public virtual DbSet<ProductInventorySnapshot> ProductInventorySnapshots { get; set; }
+    public virtual DbSet<SalesOrder> SalesOrders { get; set; }
+    public virtual DbSet<SalesOrderItem> SalesOrderItems { get; set; }
   }
 }
