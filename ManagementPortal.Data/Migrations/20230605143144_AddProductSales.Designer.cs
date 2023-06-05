7327e0fa0c4c2fb7244d3387892eb46607e48f7b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-05 11:32:27 -0300

    add product and sales migrations

--- ManagementPortal.Data/Migrations/20230605143144_AddProductSales.Designer.cs
+++ ManagementPortal.Data/Migrations/20230605143144_AddProductSales.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ManagementPortal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ManagementPortal.Data.Migrations
 {
     [DbContext(typeof(DataDbContext))]
-    partial class DataDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230605143144_AddProductSales")]
+    partial class AddProductSales
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
