5797f0f5899207b2a1e69fe3e4fef074e0891c52
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-21 14:51:55 -0300

    get inventory snapshot history for charts

--- ManagementPortal.Web/Controller/InventoryController.cs
+++ ManagementPortal.Web/Controller/InventoryController.cs
@@ -2,6 +2,7 @@ using ManagementPortal.Services.Inventory;
 using ManagementPortal.Web.Serialization;
 using ManagementPortal.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using SolarCoffee.Web.ViewModels;
 
 namespace ManagementPortal.Web.Controller
 {
@@ -51,6 +52,49 @@ namespace ManagementPortal.Web.Controller
       var adjustment = shipment.Adjustment;
       var inventory = _inventoryService.UpdateUnitsAvailable(id, adjustment);
       return Ok(inventory);
+    }
+
+    [HttpGet("/api/inventory/snapshot")]
+    public ActionResult GetSnapshotHistory()
+    {
+
+      _logger.LogInformation("Getting snapshot history");
+
+      try
+      {
+        var snapshotHistory = _inventoryService.GetSnapshotHistory();
+
+        // Get distinct points in time a snapshot was collected
+        var timelineMarkers = snapshotHistory
+            .Select(t => t.SnapshotTime)
+            .Distinct()
+            .ToList();
+
+        // Get quantities grouped by id.
+        var snapshots = snapshotHistory
+            .GroupBy(hist => hist.Product, hist => hist.QuantityOnHand,
+                (key, g) => new ProductInventorySnapshotModel
+                {
+                  ProductId = key.Id,
+                  QuantityOnHand = g.ToList()
+                })
+            .OrderBy(hist => hist.ProductId)
+            .ToList();
+
+        var viewModel = new SnapshotResponse
+        {
+          Timeline = timelineMarkers,
+          ProductInventorySnapshots = snapshots
+        };
+
+        return Ok(viewModel);
+      }
+      catch (Exception e)
+      {
+        _logger.LogError("Error getting snapshot history.");
+        _logger.LogError(e.StackTrace);
+        return BadRequest("Error retrieving history");
+      }
     }
   }
 }
